af33ff51a5ed2a585940726a2286ebf919b9e918
Sriram Anne <sriram.anne@ncr.com>
2019-07-29 19:38:08 -0400

    merge conflicts

--- tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/TweatsBackOfficeApplication.java
+++ tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/TweatsBackOfficeApplication.java
@@ -1,8 +1,4 @@
 package com.ncr.tr.helios.tweatsbackoffice;
-
-import com.ncr.tr.helios.tweatsbackoffice.customer.Customer;
-import com.ncr.tr.helios.tweatsbackoffice.customer.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,31 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TweatsBackOfficeApplication implements CommandLineRunner {
 
-    @Autowired
-    CustomerRepository repository;
-
     public static void main(String[] args) {
         SpringApplication.run(TweatsBackOfficeApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-
-        repository.deleteAll();
-
-        Customer c1 = new Customer();
-        Customer c2 = new Customer();
-
-        c1.setName("Asheton Harrell");
-
-        c2.setName("Ram Anne");
-
-        repository.save(c1);
-        repository.save(c2);
-
-        System.out.println("Here are saved customers");
-        repository.findAll().stream().forEach(System.out::println);
 
     }
 
