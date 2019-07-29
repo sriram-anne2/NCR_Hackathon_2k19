fc31e177a81a5924a072367c7c1b829723c56aa7
Asheton Harrell <ashetonharrelll@ashetonrellsmbp.corp.ncr.com>
2019-07-29 11:11:21 -0400

    init back-office

---
+++ tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/TweatsBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.ncr.tr.helios.tweatsbackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweatsBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweatsBackOfficeApplication.class, args);
+	}
+
+}
---
+++ tweats-back-office/src/test/java/com/ncr/tr/helios/tweatsbackoffice/TweatsBackOfficeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ncr.tr.helios.tweatsbackoffice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TweatsBackOfficeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
