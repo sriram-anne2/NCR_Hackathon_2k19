e6035b527453ada44a8b7857efef3a316e7ec61e
Asheton Harrell <ashetonharrelll@ashetonrellsmbp.corp.ncr.com>
2019-07-30 17:32:10 -0400

    trying twitter dm

--- tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/entities/processor/Processor.java
+++ tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/entities/processor/Processor.java
@@ -5,5 +5,5 @@ import com.ncr.tr.helios.tweatsbackoffice.entities.orders.Orders;
 public interface Processor {
 
 
-    Orders processRequestForOrder(String emojiUnicode, String twitterHandle);
+    Orders processRequestForOrder(String emojiUnicode, String twitterHandle, long recipientId);
 }
--- tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/entities/processor/ProcessorImpl.java
+++ tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/entities/processor/ProcessorImpl.java
@@ -5,39 +5,61 @@ import com.ncr.tr.helios.tweatsbackoffice.entities.customer.CustomerService;
 import com.ncr.tr.helios.tweatsbackoffice.entities.customer.FavOrders;
 import com.ncr.tr.helios.tweatsbackoffice.entities.orders.OrderService;
 import com.ncr.tr.helios.tweatsbackoffice.entities.orders.Orders;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
+import twitter4j.DirectMessage;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 
 @Service
+@Slf4j
 public class ProcessorImpl implements Processor {
 
-    @Autowired private CustomerService customerService;
+    @Autowired
+    private CustomerService customerService;
 
-    @Autowired private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
 
     @Override
-    public Orders processRequestForOrder(String emojiUnicode, String twitterHandle) {
-       // check customer service
-       CustomerInfo c1 =  customerService.getCustomerByHandle(twitterHandle);
+    public void processRequestForOrder(String emojiUnicode, String twitterHandle, long recipientId) {
+        // check customer service
+        CustomerInfo c1 = customerService.getCustomerByHandle(twitterHandle);
 
         // if valid create order w/ order service.
 
-        for(FavOrders favOrders: c1.getFavOrders()) {
+        Orders order = null;
+        for (FavOrders favOrders : c1.getFavOrders()) {
 
-            if(emojiUnicode.equalsIgnoreCase(favOrders.getEmojiUnicode())) {
-                Orders order = new Orders();
+            if (emojiUnicode.equalsIgnoreCase(favOrders.getEmojiUnicode())) {
+                order = new Orders();
                 order.setOrderDescription(favOrders.getFavOrderDescription());
                 order.setOrderedBy(c1.getCustomerTwitterHandle());
-                return orderService.addOrders(order);
-                // in addOrders we call save method for db.
+                orderService.addOrders(order);
             }
         }
 
-        return null;
+        try {
+            sendConfirmationOrErrorDirectMessage(order, recipientId);
+        } catch (TwitterException e) {
+            log.error("Cannot send direct message to sender, exception {}", e);
+        }
+    }
+
+    private void sendConfirmationOrErrorDirectMessage(Orders order, long recipientId) throws TwitterException {
+
+        Twitter sender = TwitterFactory.getSingleton();
+        DirectMessage dm;
+        if (order != null) {
+            dm = sender.sendDirectMessage(recipientId, "Your order has been confirmed!");
+            log.info("Message to be sent to recipientId {} , {}", recipientId, dm);
+        } else {
+            dm = sender.sendDirectMessage(recipientId, "Your order cannot be processed, please try again or contact support");
+            log.info("Message to be sent to recipientId {} , {}", recipientId, dm);
+        }
     }
 }
