0e9a08e062baaf8b369843c7ee7a5bd5e46b9ce8
Asheton Harrell <ashetonharrelll@ashetonrellsmbp.corp.ncr.com>
2019-07-31 15:53:42 -0400

    adding order data to view

--- tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/TweatsBackOfficeApplication.java
+++ tweats-back-office/src/main/java/com/ncr/tr/helios/tweatsbackoffice/TweatsBackOfficeApplication.java
@@ -3,6 +3,7 @@ package com.ncr.tr.helios.tweatsbackoffice;
 import com.ncr.tr.helios.tweatsbackoffice.entities.customer.CustomerInfo;
 import com.ncr.tr.helios.tweatsbackoffice.entities.customer.CustomerRepository;
 import com.ncr.tr.helios.tweatsbackoffice.entities.customer.FavOrders;
+import com.ncr.tr.helios.tweatsbackoffice.entities.orders.Orders;
 import com.ncr.tr.helios.tweatsbackoffice.entities.orders.OrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
@@ -32,6 +34,32 @@ public class TweatsBackOfficeApplication implements CommandLineRunner {
         // Database initialization I guess? clearing all previous states?
         customerRepository.deleteAll();
         ordersRepository.deleteAll();
+
+        Orders o1 = new Orders();
+        o1.setOrderedBy("@AshetonHarrell");
+        o1.setOrderDescription("Double steak bowl with guacamole, cheese, mild salsa, hot salsa, brown rice");
+        o1.setOrderTotalPrice(10.45);
+        Orders o2 = new Orders();
+        o2.setOrderedBy("@StevenKing");
+        o2.setOrderDescription("Four mini chicken quesadillas");
+        o2.setOrderTotalPrice(5.54);
+        Orders o3 = new Orders();
+        o3.setOrderedBy("RamAnne");
+        o3.setOrderDescription("Grilled chicken and mashed potatoes");
+        o3.setOrderTotalPrice(8.56);
+        Orders o4 = new Orders();
+        o4.setOrderedBy("@KevinPerry");
+        o4.setOrderDescription("Lamb gyro with hummus");
+        o4.setOrderTotalPrice(8.99);
+
+        List<Orders> orders = new ArrayList<>();
+        orders.add(o1);
+        orders.add(o2);
+        orders.add(o3);
+        orders.add(o4);
+
+        ordersRepository.saveAll(orders);
+
 
         CustomerInfo c1 = new CustomerInfo();
         c1.setCustomerName("John Newsome");
