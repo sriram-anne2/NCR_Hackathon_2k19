6703028d9455439a73e5e4ef1e205ae94227ddf8
Kevin Perry <kevin.perry@ncr.com>
2019-07-31 17:45:18 -0400

    Add Twitter DMs for order confirmation and error

--- twitter-monitor/index.js
+++ twitter-monitor/index.js
@@ -1,4 +1,7 @@
 const ORDER_HASHTAG = '#tweet2order';
+const ORDER_URL = 'http://localhost:8080/orders';
+const VALID_EMOJIS_PAYLOAD = 'saladUnicode + chickenUnicode';
+const INVALID_EMOJIS_PAYLOAD = 'invalid';
 
 const twit = require('twit');
 const axios = require('axios');
@@ -15,24 +18,50 @@ let t = new twit({
 let stream = t.stream('statuses/filter', { track: ORDER_HASHTAG });
 
 stream.on('tweet', function(tweet) {
+	let emojis;
 	console.log(tweet);
-	axios
-		.post('localhost:8080/orders', {
-			twitterHandle: tweet.user.sceen_name,
-			recipientId: tweet.user.id_str,
-			emojiCode: getEmojis(tweet.text)
-		})
-		.then(function(response) {
-			console.log(response);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
+	if (getEmojis(tweet.text) === '🥗🍗') {
+		emojis = VALID_EMOJIS_PAYLOAD;
+	} else {
+		emojis = INVALID_EMOJIS_PAYLOAD;
+	}
+	postOrder(ORDER_URL, '@' + tweet.user.screen_name, emojis, tweet.user.id_str);
 });
 
-function getEmojis(text) {
-	return text.match(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g);
+function postOrder(url, screenName, emojis, id) {
+	axios
+		.post(url, {
+			twitterHandle: screenName,
+			emojiCode: emojis
+		})
+		.then(function(response) {
+			const msg = `Thank you for your order.\n\nOrder ID:  ${response.data.orderId}\nDescription:  ${response.data
+				.orderDescription}\nTotal:  $${response.data.orderTotalPrice}`;
+			sendDirectMessage(id, msg);
+		})
+		.catch(function(error) {
+			const msg = "We're sorry, but we were unable to locate that order.  Have you set this order as a favorite?";
+			sendDirectMessage(id, msg);
+			console.log(error);
+		});
 }
 
-// let emojis = '🍩🍭🍪🍊🌮';
-// console.log(getEmojis(emojis));
+function sendDirectMessage(userId, msg) {
+	const data = {
+		event: {
+			type: 'message_create',
+			message_create: { target: { recipient_id: userId }, message_data: { text: msg } }
+		}
+	};
+	t.post('direct_messages/events/new', data, function(err, data, response) {
+		if (err !== undefined) {
+			console.error(err);
+		}
+	});
+}
+
+function getEmojis(text) {
+	return text
+		.match(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g)
+		.join('');
+}
